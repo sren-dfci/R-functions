2b78dcce5b0aa67a7424d5a71dcf89df4f1a36e8
sren-dfci <sren@ds.dfci.harvard.edu>
2021-02-25 19:50:22 -0500

    add competing risk 95 percent CI

---
+++ cifCI.R
@@ -0,0 +1,14 @@
+"""
+cmprsk package only returns point estimate and variance for each time point,
+this function calculates the 95% CI with an assigned time point.
+"""
+cifCI <- function(.cuminc, .tp) {
+  require(cmprsk)
+  .ls <- timepoints(.cuminc, .tp) # a list
+  .df <- data.frame(.ls[["est"]], .ls[["var"]])
+  colnames(.df) <- c("est", "var")
+  row_names <- rownames(.df)
+  .df$lower <- .df$est - 1.96 * sqrt(.df$var)
+  .df$upper <- .df$est + 1.96 * sqrt(.df$var)
+  return(.df)
+}
