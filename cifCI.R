779e31e1ecde40b6090440a017ae1c5e6a031449
sren-dfci <sren@ds.dfci.harvard.edu>
2021-04-12 16:27:11 -0400

    minor change

--- cifCI.R
+++ cifCI.R
@@ -3,7 +3,6 @@ cifCI <- function(.cuminc, .tp) {
   .ls <- timepoints(.cuminc, .tp) # a list
   .df <- data.frame(.ls[["est"]], .ls[["var"]])
   colnames(.df) <- c("est", "var")
-  row_names <- rownames(.df)
   .df$lower <- .df$est - 1.96 * sqrt(.df$var)
   .df$upper <- .df$est + 1.96 * sqrt(.df$var)
   return(.df)
